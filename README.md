d88ff7e17548749dd73aa348b9b715e1e96e627f
dvoiss <davidvoiss@gmail.com>
2012-02-12 22:38:26 -0600

    Updated repo

---
+++ README.md
@@ -0,0 +1,30 @@
+# A script to run before I go to the library...
+
+I wrote this after going to the library to return a book, at which point I needed to check my Amazon wishlist (which serves as my reading to-do list) to see what to get next. Naturally going to a smaller branch of the Chicago Public Library meant 90% of the items on my list weren't stocked at this location or were checked out.
+
+This script grabs my Amazon wishlist, filters by books, and uses [Nokogiri](http://www.nokogiri.org) to parse the books into a collection (Amazon does not expose wishlists via an api). The ISBN is parsed from Amazon and sent to [Library Thing](http://www.librarything.com)'s ISBN api to get related ISBN numbers for the same book&em;when a book has a new edition it is given a different ISBN number (some books on my wishlist have 20+ numbers O_o).
+
+Finally the retrieved ISBNs are fed into the [Chicago Public Library](http://www.chipublib.org) search, this is done 5 ISBNs at a time as there is apparently a limit (though it isn't stated anywhere, 5 to 10 ISBNs at once seems to result in 0 results even if they are in the library's system). If a "detail" result page is retrieved, we parse it to see if the book is checked out. If a search results page is retrieved, we parse it and make a request for each result to see the result's detail page so we can determine whether it is checked out or not.
+
+# Usage
+
+Add your email and leave the library-id blank to search entire system. Todo: make these ids available so they don't need to be looked up at [chipublib.org](chipublib.org)..
+
+  $ ruby search.rb 
+    Usage: search.rb [email] [library-id]
+
+    Library id can be left blank, otherwise an id is needed corresponding to
+    the library you want, example: 70 for Chinatown, 320 for Bucktown-Wicker park
+
+# Example
+
+  $ ruby search.rb me@email.com 320
+    Retrieving wishlist
+    Finding books...
+
+    The Prince (Bantam Classics) is available at your library.
+    Boneshaker (Sci Fi Essential Books) is available at your library.
+    Where Good Ideas Come From: The Natural History of Innovation is available at your library.
+    River of Gods is available at your library.
+    The Name of the Rose: including the Author's Postscript is available at your library.
+    Foucault's Pendulum is available at your library.
---
+++ search.rb
@@ -0,0 +1,227 @@
+require 'net/https'
+require "open-uri"
+require 'uri'
+require 'zlib'
+
+require "nokogiri"
+
+# the library thing isbn api allows us to get related ISBNs,
+# so if we're searching for a book and the library has an older or newer
+# edition or a reprint, etc. we can search for that as well
+LIBRARY_THING_ISBN_URL = "http://www.librarything.com/api/thingISBN/"
+
+# library constants
+LIBRARY_BASE_URL = "http://www.chipublib.org"
+LIBRARY_SEARCH_URL = "#{LIBRARY_BASE_URL}/search/results/"
+LIBRARY_REFERER_URL = "#{LIBRARY_BASE_URL}/search/advanced/"
+LIBRARY_NO_RESULTS_STRING = "Your search did not produce any results."
+LIBRARY_MY_STRING = "My Library"
+LIBRARY_NOT_CHECKED_OUT = "Not checked out"
+
+# loop through the pages of the wishlist,
+# on the first time through after we receive the first page, we'll grab the
+# maximum number of pages
+def get_wishlist(email)
+  books = []
+
+  puts "Retrieving wishlist"
+
+  uri = URI.parse("http://www.amazon.com/registry/search.html?type=wishlist&filter=3&layout=compact&field-name=#{email}")
+  uri_path = "#{uri.path}?#{uri.query}"
+  request = Net::HTTP::Get.new(uri_path)
+  response = Net::HTTP.start(uri.host, uri.port) do |http|
+    http.request(request)
+  end
+  wishlist_url = response['location']
+
+  if wishlist_url.nil?
+    puts "Cannot find the wishlist for #{email}"
+    exit
+  end
+
+  page = Nokogiri::HTML(open("#{wishlist_url}&filter=3&layout=compact"))
+
+  # get the divs and parse out their title and author
+  page.css('tbody[class=itemWrapper]').each do |part|
+    link_with_isbn = part.css('span[class="small productTitle"] a')
+    link = link_with_isbn.length > 0 ? link_with_isbn.first : nil
+    if link.nil?; next end
+
+    # get the href attribute and try to get a match for the ISBN
+    isbn_available = link.attr('href').match(/dp\/([\d\w]+)\//)
+    # did we get an ISBN?
+    if isbn_available && isbn_available.captures.one?
+      isbn = isbn_available.captures.pop
+      title = link_with_isbn.text.strip
+      books.push({ :title => title, :isbn => isbn })
+
+      # right now, I'm not using author:
+      next
+
+      # grab the author, eliminating the " by " text if it exists,
+      # also eliminate the special characters
+      authors = part.css('span[class="tiny"]').first.text.gsub(/\s*by\s+/, '').match(/([\w\s\'\-\.]+)/)
+      if authors and not authors.captures.empty?
+        # temporarily use the first author
+        books.push({ :author => authors.captures.first.strip, :title => title, :isbn => isbn })
+      end
+    end
+  end
+
+  books
+end
+
+# for fetching the library pages
+def fetch(uri_str, limit = 5, page_type = "search")
+  if limit > 0
+    uri = URI.parse(uri_str)
+
+    # header
+    header = {
+      "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.75 Safari/535.7",
+      "Referer" => LIBRARY_REFERER_URL,
+      "Host" => uri.host,
+      "Accept-Encoding" => "gzip,deflate,sdch"
+    }
+
+    # reference: http://ruby-doc.org/stdlib/libdoc/net/http/rdoc/classes/Net/HTTP.html
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request.initialize_http_header(header)
+    response = Net::HTTP.start(uri.host, uri.port) { |http| http.request(request) }
+    case response
+    when Net::HTTPSuccess     then { :response => response, :page_type => page_type }
+    when Net::HTTPRedirection then fetch("#{LIBRARY_BASE_URL + response['location']}", limit - 1, "detail")
+    else
+      response.error!
+    end
+  else
+    # todo
+    raise "TOO MANY REDIRECTS"
+  end
+end
+
+# use librarything's ISBN api to retrieve related ISBNs,
+# so we don't miss the book we want because of a reprint or different edition,
+# one book can have many ISBNs as a result of different versions, etc.
+def get_related_isbns(isbn)
+  response = open("http://www.librarything.com/api/thingISBN/" + isbn)
+  if response.nil?; return [isbn] end
+
+  page = Nokogiri::XML(response)
+  related_isbns = []
+  page.css('isbn').each { |related_isbn| related_isbns.push(related_isbn.text) }
+
+  related_isbns
+end
+
+# parse a "detail page" (page which results when a book has been found)
+def parse_detail(body)
+  libraries = []
+
+  page = Nokogiri::HTML(body)
+  page.css('table[class=summary] tr').each do |tablerow|
+    if tablerow.to_s.index(LIBRARY_NOT_CHECKED_OUT) != nil
+      # it's at our library, break out of loop
+      if LIBRARY_BRANCH_LOCATION != nil && tablerow.previous_sibling.to_s.index("Your Library") != nil 
+        return [LIBRARY_MY_STRING]
+      end
+
+      # it isn't checked out at this location, save library name
+      libraries.push tablerow.css('td').first.text
+    end
+  end
+
+  libraries
+end
+
+# get the libraries the book is available at, if it is available at the library
+# specified by LIBRARY_MY_STRING, then just return that library
+def available_at(page)
+  libraries_available = []
+  links = page.css("ol[class=result] li[class=clearfix] h3 a")
+  links.each do |link|
+    fetch_result = fetch("#{LIBRARY_BASE_URL + (link.attr 'href')}")
+    body = Zlib::GzipReader.new(StringIO.new(fetch_result[:response].body)).read
+    libraries_available.concat parse_detail(body)
+
+    # should we go to the next link? do we already know if it's available at our library?
+    if libraries_available.include? LIBRARY_MY_STRING; return [LIBRARY_MY_STRING] end
+  end
+
+  libraries_available
+end
+
+# go through books and tell me if they are available at my local library
+def find_books(books, library)
+  puts "Finding books..."
+
+  books.each do |book|
+    # get related isbns and limit collection to a maximum of 10 ISBNs
+    related_isbns = get_related_isbns(book[:isbn])[0...10]
+
+    libraries_available = []
+    # search through ISBNs, 5 at a time (due to limits on chipublib search),
+    # break at first results found
+    (0...related_isbns.length).step(5) do |count|
+      isbn_search_range = related_isbns[count...count+5]
+      isbn_search_string = isbn_search_range.join('+or+')
+
+      fetch_result = fetch("#{LIBRARY_SEARCH_URL}?&isbn=#{isbn_search_string}&location=#{LIBRARY_BRANCH_LOCATION}&format=Book&advancedSearch=submitted")
+
+      # unzip
+      body = Zlib::GzipReader.new(StringIO.new(fetch_result[:response].body)).read
+
+      if fetch_result[:page_type] == "search"
+        if body.index(LIBRARY_NO_RESULTS_STRING) == nil
+          # assemble a collection of results
+          page = Nokogiri::HTML(body)
+          libraries_available = available_at(page)
+        else
+          # puts LIBRARY_NO_RESULTS_STRING
+        end
+      else # detail
+        libraries_available = parse_detail(body)
+      end
+
+      if libraries_available.include? LIBRARY_MY_STRING; break end
+
+      # don't make too many requests too fast :)
+      # sleep 1/10
+    end
+
+    # show where the book is available, if it isn't available, output nothing
+    if libraries_available.include? LIBRARY_MY_STRING
+      puts "\033[33m#{book[:title]}\033[0m is available at your library."
+    elsif libraries_available.length > 0 && LIBRARY_BRANCH_LOCATION == ''
+      puts "\033[33m#{book[:title]} is available at other libraries: #{libraries_available.uniq.join(', ')}"
+    else
+      # puts "#{book[:title]} is not available."
+    end
+  end
+end
+
+unless ARGV.length == 1 || ARGV.length == 2
+  puts "Usage: #{$0} [email] [library-id]"
+  puts "Library id can be left blank, otherwise an id is needed corresponding to"
+  puts "the library you want, example: 70 for Chinatown, 320 for Bucktown-Wicker park"
+  puts "defaults to Bucktown-Wicker Park (ids are from chipublib.org's catalog search)"
+  exit
+end
+
+email = ARGV[0]
+
+case ARGV[1]
+when nil then library = ''
+else library = ARGV[1]
+end
+
+LIBRARY_BRANCH_LOCATION = library
+
+# TODO:
+# Sinatra-fy into heroku app so I can use on my phone
+# - due to the many requests that need to be made, 
+# - combined with the scraping/parsing,
+# - it is probably too slow to be useful
+
+books = get_wishlist(email)
+find_books(books, library)
